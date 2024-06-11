a1a302867f2be5cd2cb39cbebe4fe80cf7e05e74
Ignacio Alesina <ignacio.alesina@xmartlabs.com>
2024-06-11 10:47:26 -0300

    http methods

--- src/networking/controllers/todos.ts
+++ src/networking/controllers/todos.ts
@@ -1,24 +1,45 @@
 import { Todo } from '../../types/globalTypes'
 import { httpService } from '../httpService'
-interface TodoResponse {
+export interface TodoResponse {
   userId: number
   id: number
   title: string
   completed: boolean
 }
+function serialization(todo: Todo): TodoResponse {
+  return {
+    completed: todo.checked,
+    id: todo.id,
+    title: todo.title,
+    userId: 1,
+  }
+}
+
+function deserialization(value: TodoResponse): Todo {
+  return {
+    checked: value.completed,
+    description: 'Description of: ' + value.title,
+    id: value.id,
+    title: value.title,
+  }
+}
 
 async function getTodos(): Promise<Todo[]> {
   const response = await httpService.get<TodoResponse[]>('/todos')
-  return response.map(todo => ({
-    checked: todo.completed,
-    description: 'Description of: ' + todo.title,
-    id: todo.id,
-    title: todo.title,
-  }))
+  return response.map(todo => deserialization(todo))
 }
 
-async function postNewTodo(title: string) {
-  const response = await httpService.post<TodoResponse>('/todos')
+async function postNewTodo(title: string, description: string): Promise<Todo> {
+  const response = await httpService.post<TodoResponse>('/todos', title)
+  return deserialization(response)
 }
 
-export const todosController = { getTodos, postNewTodo }
+async function modifyTodo(todo: Todo) {
+  const response = await httpService.put<TodoResponse>(
+    '/todos/' + todo.id,
+    serialization(todo),
+  )
+  return response
+}
+
+export const todosController = { getTodos, modifyTodo, postNewTodo }
--- src/networking/httpService.ts
+++ src/networking/httpService.ts
@@ -1,3 +1,5 @@
+import { TodoResponse } from './controllers/todos'
+
 const baseUrl = 'https://jsonplaceholder.typicode.com'
 
 async function get<T = any>(endpoint: string): Promise<T> {
@@ -17,7 +19,7 @@ async function post<T = any>(endpoint: string, title: string): Promise<T> {
   try {
     const response = await fetch(baseUrl + endpoint, {
       body: JSON.stringify({
-        body: '',
+        completed: false,
         title: title,
         userId: 1,
       }),
@@ -27,7 +29,7 @@ async function post<T = any>(endpoint: string, title: string): Promise<T> {
       method: 'POST',
     })
     if (!response.ok) {
-      throw new Error('Failed to fetch todos')
+      throw new Error('The new todo could not be uploaded')
     }
     const data = await response.json()
     return data as T
@@ -36,7 +38,31 @@ async function post<T = any>(endpoint: string, title: string): Promise<T> {
   }
 }
 
+async function put<T = any>(endpoint: string, params: TodoResponse) {
+  try {
+    const response = await fetch(baseUrl + endpoint, {
+      body: JSON.stringify({
+        completed: params.completed,
+        id: params.id,
+        title: params.title,
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      method: 'PUT',
+    })
+    if (!response.ok) {
+      throw new Error('') //No error message so todos continue appearing, problem that error is not shown
+    }
+    const data = await response.json()
+    return data as T
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
 export const httpService = {
   get,
   post,
+  put,
 }
